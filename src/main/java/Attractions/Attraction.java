19e5bdcb66829b690f62421d64b47de7adcf0550
Mark <nirvanafoo86@hotmail.com>
2019-05-14 20:15:39 +0100

    Creating Themepark and Attraction Classes

---
+++ src/main/java/Attractions/Attraction.java
@@ -0,0 +1,11 @@
+package Attractions;
+
+public abstract class Attraction extends ThemePark {
+
+    private int rating;
+
+    public Attraction(String name, int rating){
+        super(name);
+        this.rating
+    }
+}
---
+++ src/main/java/ThemePark.java
@@ -0,0 +1,11 @@
+//import java.util.ArrayList;
+
+public class ThemePark {
+
+    private String name;
+//    private ArrayList<IReviewed>();
+
+    public ThemePark(String name){
+        this.name = name;
+    }
+}

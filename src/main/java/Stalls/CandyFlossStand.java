059faa0c3c053208f159a538b3704138e63a4cde
Mark <nirvanafoo86@hotmail.com>
2019-05-14 21:30:28 +0100

    Adding CandyFlossStand class

---
+++ src/main/java/Stalls/CandyFlossStand.java
@@ -0,0 +1,8 @@
+package Stalls;
+
+public class CandyFlossStand extends Stall {
+
+    public CandyFlossStand(String name, String ownerName, int parkingSpace, int rating){
+        super(name, ownerName, parkingSpace, rating);
+    }
+}

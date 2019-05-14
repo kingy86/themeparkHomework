53ce46dbf72f5e5e2552f44f56315793ddc041b1
Mark <nirvanafoo86@hotmail.com>
2019-05-14 20:46:33 +0100

    passed tests in DodgemTest file

--- src/main/java/Attractions/Dodgems.java
+++
@@ -1,8 +0,0 @@
-package Attractions;
-
-public class Dodgems extends Attraction {
-
-    public Dodgems(String name, int rating){
-        super(name, rating);
-    }
-}
--- src/test/java/DodgemTest.java
+++ src/test/java/DodgemTest.java
@@ -1,2 +1,26 @@
+import Attractions.Dodgem;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DodgemTest {
+
+
+    Dodgem dodgem;
+
+    @Before
+    public void before(){
+        dodgem = new Dodgem("Bumper Cars", 6);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Bumper Cars", dodgem.getName());
+    }
+
+    @Test
+    public void hasRating(){
+        assertEquals(6, dodgem.getRating());
+    }
 }

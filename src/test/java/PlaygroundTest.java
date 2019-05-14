9c219c87aec7eee7081fb650e2251a7a7113864c
Mark <nirvanafoo86@hotmail.com>
2019-05-14 20:56:52 +0100

    Passed hasName test

--- src/test/java/PlaygroundTest.java
+++ src/test/java/PlaygroundTest.java
@@ -1,2 +1,20 @@
+import Attractions.Playground;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PlaygroundTest {
+
+    Playground playground;
+
+    @Before
+    public void before(){
+        playground = new Playground("Woodland Area", 8);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Woodland Area", playground.getName());
+    }
 }

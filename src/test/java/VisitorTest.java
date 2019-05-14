f53808be979ce86bef8dbb75ef5f26bb5d631939
Mark <nirvanafoo86@hotmail.com>
2019-05-14 21:51:01 +0100

    Visitor passed hasAge test

---
+++ src/test/java/VisitorTest.java
@@ -0,0 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class VisitorTest {
+
+    Visitor visitor;
+
+    @Before
+    public void before(){
+        visitor = new Visitor(16, 175, 20.00);
+    }
+
+    @Test
+    public void hasAge(){
+        assertEquals(16, visitor.getAge());
+    }
+}
